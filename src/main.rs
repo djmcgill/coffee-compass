ceb8f996ee93d62b0a9e7eea582f788f6518fb2f
David McGillicuddy <contact@djmcgill.co.uk>
2018-07-15 01:29:09 +0100

    add core::fmt::Write implementation

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate nb;
 
 use hal::hal::blocking::i2c::WriteRead;
 use hal::prelude::*;
+use core::fmt::Write;
 
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
@@ -34,36 +35,39 @@ fn main() {
 
     let mut pins = peripherals.PORT.split();
     let gclk0 = clocks.gclk0().clone();
-//    let mut i2c = I2CMaster0::new(
-//        &clocks.sercom0_core(&gclk0).unwrap(),
-//        400.khz(),
-//        peripherals.SERCOM0,
-//        &mut peripherals.PM,
-//        pins.pa8.into_pad(&mut pins.port),
-//        pins.pa9.into_pad(&mut pins.port),
-//    );
+    let mut i2c = I2CMaster0::new(
+        &clocks.sercom0_core(&gclk0).unwrap(),
+        400.khz(),
+        peripherals.SERCOM0,
+        &mut peripherals.PM,
+        pins.pa8.into_pad(&mut pins.port),
+        pins.pa9.into_pad(&mut pins.port),
+    );
 
     let rx_pin = pins.pb23.into_pull_down_input(&mut pins.port).into_pad(&mut pins.port);
     let tx_pin = pins.pb22.into_push_pull_output(&mut pins.port).into_pad(&mut pins.port);
-    let mut serial = UART5::new(
+    let mut serial = BlockingWrite(UART5::new(
         &clocks.sercom5_core(&gclk0).unwrap(),
         9600.hz(),
         peripherals.SERCOM5,
         &mut core.NVIC,
         &mut peripherals.PM,
-        UART5Pinout::Rx3Tx2 {rx: rx_pin, tx: tx_pin});
+        UART5Pinout::Rx3Tx2 {rx: rx_pin, tx: tx_pin}));
 
-
+    serial.0.bwrite_all(b"pre-starting\n").unwrap();
+    serial.write_str("starting\n").unwrap();
     let mut builtin_led = pins.pb8.into_open_drain_output(&mut pins.port);
 
     let mut led_state = true;
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
-//    let mut data = [0u8];
-//    i2c.write_read(MAG_ADDRESS, &[MAG_DEVICE_ID], &mut data).unwrap();
-//    let digits = [(data[0] & 0b1111_0000) >> 4, data[0] & 0b0000_1111];
-    // let digits = [0x01, 0x0F];
-//    let mut digit_index = 0usize;
+    let mut data = [0u8];
+    serial.write_str("reading\n").unwrap();
+    let res = i2c.write_read(MAG_ADDRESS, &[MAG_DEVICE_ID], &mut data);
+    serial.write_fmt(format_args!("WHOAMI {:?}\n", res)).unwrap();
+    serial.write_fmt(format_args!("done printing: {:?}\n", data[0])).unwrap();
+//    let foo = format_args!("{}", data[0]);
+//    serial.bwrite()
 
     // Set by pass mode for the magnetometers
     //  I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
@@ -111,7 +115,7 @@ fn main() {
 //   Serial.print ("\t");
   
 //        if digit_index == 0 || digit_index == 1 {
-//            eight_segment.display(digits[digit_index], false);
+//            eight_s=gment.display(digits[digit_index], false);
 //            digit_index += 1;
 //        } else if digit_index >= 2 {
 //            eight_segment.blank();
@@ -132,13 +136,21 @@ fn main() {
 //        }
 //        serial.write(b"foo2\n").unwrap();
 
-        for byte in b"Hello, world!" {
-            block!(serial.write(*byte)).unwrap();
+//        serial.bwrite_all(b"Hello, world!").unwrap();
+        delay.delay_ms(200u8);
+        delay.delay_ms(200u8);
+        delay.delay_ms(200u8);
+        delay.delay_ms(200u8);
+        delay.delay_ms(200u8);
+    }
+}
+
+struct BlockingWrite<W>(W) where W: hal::hal::blocking::serial::Write<u8>;
+impl<W: hal::hal::blocking::serial::Write<u8>> core::fmt::Write for BlockingWrite<W> {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        match self.0.bwrite_all(s.as_bytes()) {
+            Ok(()) => Ok(()),
+            Err(_) => Err(core::fmt::Error),
         }
-        delay.delay_ms(200u8);
-        delay.delay_ms(200u8);
-        delay.delay_ms(200u8);
-        delay.delay_ms(200u8);
-        delay.delay_ms(200u8);
     }
 }
